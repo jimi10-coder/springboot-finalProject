444ee0e44f672f11c3f461d09e993f40a30d3786
jimi10-coder <jimijini1007@gmail.com>
2025-05-22 15:03:15 +0200

    first commit

---
+++ src/main/java/pl/edu/vistula/first_project_java_spring_Djihed/FirstProjectJavaSpringDjihedApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring_Djihed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringDjihedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpringDjihedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/first_project_java_spring_Djihed/FirstProjectJavaSpringDjihedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring_Djihed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaSpringDjihedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
